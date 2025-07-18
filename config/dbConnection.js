f2d83960f6f154b7b270b387c9fdae73f8534eec
exquisite-duck <mohd.sohail.developer@gmail.com>
2025-07-18 09:45:02 +0530

    removed sync from db conn

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -24,15 +24,6 @@ try {
 } catch (error) {
   console.error("DB connection ain't shit bro: ", error);
 }
-;
-
-sequelize.sync({ force: false }) // Set `force: true` to drop and recreate tables
-    .then(() => {
-        console.log("All models were synchronized successfully.");
-    })
-    .catch((error) => {
-        console.error("Error synchronizing models:", error);
-    });
 
 module.exports = sequelize;
 
