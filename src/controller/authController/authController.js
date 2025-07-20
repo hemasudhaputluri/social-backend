b1e4d5a8218d1347caaeb0560338544edc527c58
exquisite-duck <mohd.sohail.developer@gmail.com>
2025-07-20 22:02:34 +0530

    started basic auth

---
+++ src/controller/authController/authController.js
@@ -0,0 +1,69 @@
+const {
+    login,
+    register,
+    logout,
+    resetPassword,
+    changePassword,
+    verifyToken,
+    verifyEmail
+} = require('../../sequelizeControllers/authSequelizer/authSequelizer.js');
+
+const loginController = async (req, res) => {
+    try {
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+const registerController = async (req, res) => {
+    try {
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+const logoutController = (req, res) => {
+    try {
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+const resetPasswordController = async (req, res) => {
+    try {
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+const changePasswordController = async (req, res) => {
+    try {
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+const verifyTokenController = (req, res) => {
+    try {
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+const verifyEmailController = async (req, res) => {
+    try {
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+module.exports = {
+    loginController,
+    registerController, 
+    logoutController,
+    resetPasswordController,
+    changePasswordController,   
+    verifyTokenController,
+    verifyEmailController
+};
--- src/routes/allRoutes.js
+++ src/routes/allRoutes.js
@@ -1,8 +1,10 @@
 const express  = require('express');
 const postRoutes = require('./postRoutes.js');
+const authRoutes = require('./authRoutes.js');
 const router = express.Router();
 
 router.use('/posts', postRoutes);
+router.use('/auth', authRoutes);
 
 
 module.exports = router;
